de16b42cdfb39b4f361d00beeba6a41cc7bd815e
hugmouse <mysh@tuta.io>
2021-04-27 18:44:34 +0800

    Add error handling

--- api/router/index.go
+++ api/router/index.go
@@ -1,38 +1,53 @@
 package handler
 
 import (
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 const (
 	E621Url = "https://e621.net"
 )
 
+var (
+	ErrRequestFailed        = errors.New("e621 request failed")
+	ErrNotOKCode            = errors.New("e621 not ok status code returned")
+	ErrBodyReadingError     = errors.New("e621 body reading failed")
+	ErrResponseWriterFailed = errors.New("an error occurred while trying to write to the ResponseWriter")
+)
+
+// CombinedError combines multiple errors into one
+func CombinedError(errors ...string) []byte {
+	return []byte(strings.Join(errors, ". "))
+}
+
+// Handler is what does all the work
 func Handler(w http.ResponseWriter, r *http.Request) {
 	e621Resp, err := http.Get(E621Url + r.URL.Path)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte("\"/\" E621 request failed: " + err.Error()))
+		_, _ = w.Write(CombinedError(ErrRequestFailed.Error(), err.Error()))
 		return
 	}
 	if e621Resp.StatusCode != http.StatusOK {
 		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte("\"/\" E621 not ok status code returned: " + strconv.Itoa(e621Resp.StatusCode)))
+		_, _ = w.Write(CombinedError(ErrNotOKCode.Error(), strconv.Itoa(e621Resp.StatusCode)))
 		return
 	}
 	e621Info, err := ioutil.ReadAll(e621Resp.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte("\"/\" E621 body reading failed: " + err.Error()))
+		_, _ = w.Write(CombinedError(ErrBodyReadingError.Error(), err.Error()))
 		return
 	}
 	w.WriteHeader(http.StatusOK)
 	_, err = w.Write(e621Info)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte("An error occurred while trying to write to the ResponseWriter: " + err.Error()))
+		_, _ = w.Write(CombinedError(ErrResponseWriterFailed.Error(), err.Error()))
 		return
 	}
 }
